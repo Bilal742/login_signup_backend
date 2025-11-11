f16137bee5edcec4b4f306446f6f442cf77443b2
Bilal <your_email@example.com>
2025-11-11 00:07:38 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,21 @@
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
-import connectDB from "./config/db.js";
 import router from "./routes/userRoutes.js";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 
-// Middlewares
-app.use(express.json());
+// middleware
 app.use(cors());
+app.use(express.json());
 
-// ✅ Connect MongoDB
+// DB
 connectDB();
 
-// ✅ Routes
+// Routes
 app.use("/", router);
 
-// ✅ Test Route
-app.get("/", (req, res) => {
-  res.send("✅ Server is running...");
-});
-
-// ✅ Listen (ONLY FOR RAILWAY / RENDER)
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
-});
-
+export default app;
